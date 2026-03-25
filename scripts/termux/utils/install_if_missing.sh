b593f768732e7a8c3344c03ee581210a938e5fb6
rxchris <rxchris94@gmail.com>
2026-03-25 13:37:16 +0000

    feat(utils): added platform specific package installers

---
+++ scripts/termux/utils/install_if_missing.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+
+source "$(SCRIPT_DIR)/../../common/utils/cmd_install_if_missing.sh"
+
+# Wrapper function for Ubuntu (using apt)
+install_if_missing() {
+    cmd_install_if_missing "pkg install -y" "$@"
+}
+
