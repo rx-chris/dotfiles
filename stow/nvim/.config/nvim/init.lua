6fc752770179703b98f4b223dc3af2c6b2a0f80c
rxchris <rxchris94@gmail.com>
2026-03-25 13:18:28 +0000

    feat(nvim): added clipboard functionality for termux proot environment

--- stow/nvim/.config/nvim/init.lua
+++ stow/nvim/.config/nvim/init.lua
@@ -1,3 +1,4 @@
-require("core.options")	-- load options configuration
-require("core.keymaps")	-- load keymaps configuration
+require("core.options") -- load options configuration
+require("core.clipboard") -- load clipboard options (environment-specificc)
+require("core.keymaps") -- load keymaps configuration
 require("core.lazy") -- load lazy.nvim
