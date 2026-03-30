9dac18f4940905e8dcbc4d09f528c9021da0c214
rxchris <rxchris94@gmail.com>
2026-03-30 14:52:21 +0000

    refactor(utils): minor refactoring

--- scripts/platforms/ubuntu/utils/install_if_missing.sh
+++ scripts/platforms/ubuntu/utils/install_if_missing.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 
-source "$(SCRIPT_DIR)/../../common/utils/cmd_install_if_missing.sh"
+source "$SCRIPT_DIR/../../common/utils/cmd_install_if_missing.sh"
 
 # Wrapper function for Ubuntu (using apt)
 install_if_missing() {
