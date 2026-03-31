3f8062a83e6ad4ca4e8308416596b4e7a2fec59c
rxchris <rxchris94@gmail.com>
2026-03-31 06:09:53 +0000

    feat(nvim): configure neo-tree to reveal active file in file tree

--- stow/nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ stow/nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -13,9 +13,23 @@ return {
 				hide_gitignored = false, -- Optional: set to false to show gitignored files
 				visible = true, -- Ensures this setting is active
 			},
+			follow_current_file = {
+				enabled = true,
+				leave_dirs_open = false,
+			},
 		},
 	},
 	keys = {
-		{ "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Toggle Neo-tree" },
+		{
+			"<leader>e",
+			function()
+				require("neo-tree.command").execute({
+					toggle = true,
+					source = "filesystem",
+					reveal = true,
+				})
+			end,
+			desc = "Reveal current file in Neo-tree",
+		},
 	},
 }
