d8330c39e5e59f13976821240d24b8658d9370f4
rxchris <rxchris94@gmail.com>
2026-04-11 03:43:00 +0000

    fix(env): fixed runtime check error when proc/1/root does not exist

--- scripts/common/utils/checks/runtime_checks.sh
+++ scripts/common/utils/checks/runtime_checks.sh
@@ -24,22 +24,18 @@ is_proot_runtime() {
     if [ -n "${PROOT_TMP_DIR:-}" ] || [ -n "${PROOT_NO_SECCOMP:-}" ]; then
         return 0
     fi
-
     # 2. Check parent process for 'proot'
     if ps -o ppid= -p $$ | xargs ps -o cmd= -p | grep -q proot; then
         return 0
     fi
-
     # 3. Check if root filesystem differs from /proc/1/root
-    if [ "$(stat -c %d /)" != "$(stat -c %d /proc/1/root)" ]; then
+    if [ -e /proc/1/root ] && [ "$(stat -c %d /)" != "$(stat -c %d /proc/1/root)" ]; then
         return 0
     fi
-
     # 4. Check for overlay mounts
     if grep -q overlay /proc/self/mountinfo; then
         return 0
     fi
-
     # Not inside proot
     return 1
 }
